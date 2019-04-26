5c3659be7c5c94fefb0e4c4bfc8f29c04f0051b7
Dwigth <dwigth32@outlook.es>
2019-04-26 12:07:47 -0500

    Lista de libros

--- index.ts
+++ index.ts
@@ -2,5 +2,13 @@
 import './style.css';
 import { books } from './assets/booklist';
 
-const appDiv: HTMLElement = document.getElementById('app');
+const appDiv: HTMLElement = document.getElementById('title');
+const list: HTMLElement = document.querySelector('#list');
 appDiv.innerHTML = `<h1>TypeScript Pilas</h1>`;
+
+books.forEach((book:any)=>{
+    const li:HTMLElement = document.createElement('li');
+    li.classList.add('list-group-item');
+    li.textContent = book.name;
+    list.appendChild(li);
+});
